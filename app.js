a1e9707f5ef8ca32aaf61608e65db1fcdad45396
limweb <limweb@hotmail.com>
2018-03-16 16:41:15 +0700

    addlibs

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const Foo = { template: "<div>foo</div>" };
+const Bar = { template: "<div>bar</div>" };
+
+const routes = [
+    { path: "/foo", component: Foo },
+    { path: "/bar", component: Bar }
+];
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+});
+
 const store = new Vuex.Store({
     state: {
         count: 0
@@ -12,7 +24,16 @@ const store = new Vuex.Store({
 window.vm = new Vue({
     el: "#app",
     store,
-    template: `<h1>Test</h1>`,
+    router,
+    template: `
+    <div id="app">
+        <h1>Hello App!</h1>
+        <p>
+            <router-link to = "/foo" > Go to Foo </router-link> 
+            <router-link to = "/bar" > Go to Bar </router-link> 
+            </p>
+            <router-view></router-view> 
+    </div>`,
     created() {
         console.log("vue create", this);
     }
--- vendor.js
+++ vendor.js
@@ -4,8 +4,26 @@ import collect from "collect.js";
 import Vuex from "vuex";
 window.Vuex = Vuex;
 import axios from "axios";
-import ("vue-multianalytics");
-import ("vue-router");
+import VueMultianalytics from "vue-multianalytics";
+let gaConfig = {
+    appName: "Test", // Mandatory
+    appVersion: "0.1", // Mandatory
+    trackingId: "YOUR_UA", // Mandatory
+    debug: true // Whether or not display console logs debugs (optional)
+};
+let mixpanelConfig = {
+    token: "YOUR_TOKEN"
+};
+Vue.use(VueMultianalytics, {
+    modules: {
+        ga: gaConfig,
+        mixpanel: mixpanelConfig
+    }
+});
+
+import VueRouter from "vue-router";
+window.VueRouter = VueRouter;
+Vue.use(VueRouter);
 Vue.prototype.$http = axios;
 Vue.prototype.$collect = collect;
 Vue.use(Vuex);
