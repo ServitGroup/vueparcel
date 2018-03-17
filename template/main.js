5045156576e3ea02dc7e64430c17f92b21fe153e
limweb@hotmail.com <limweb@hotmail.com>
2018-03-17 21:32:15 +0700

    move  store to file and add sync stort router

--- template/main.js
+++ template/main.js
@@ -1,21 +1,13 @@
 import App from "./pages/app.js";
 import routes from "./routes/routes.js";
+import store from "./stores/index.js";
 
 const router = new VueRouter({
     mode: "history",
     routes // short for `routes: routes`
 });
 
-const store = new Vuex.Store({
-    state: {
-        count: 0
-    },
-    mutations: {
-        increment(state) {
-            state.count++;
-        }
-    }
-});
+const unsync = Vue._sync(store, router); // done. Returns an unsync callback fn
 
 window.vm = new Vue({
     el: "#app",
