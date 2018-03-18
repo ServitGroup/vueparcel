9c3b11c338b729e39b5b83d6718e23dc9297667b
limweb@hotmail.com <limweb@hotmail.com>
2018-03-19 00:53:26 +0700

    add beformtoureleave and beforrouteenter in component and mixins

--- template/pages/Contact.js
+++ template/pages/Contact.js
@@ -1,6 +1,40 @@
 export default {
     template: `<H1>contact</H1>`,
     name: "Contact",
+    beforeRouteEnter(to, from, next) {
+        console.log("route เข้า component ", this);
+        // Pass a callback to next (optional)
+        next(vm => {
+            // this callback has access to component instance (ie: 'this') via `vm`
+            vm.$nextTick(() => {
+                console.log("check vm", vm);
+                // vm.$root.$refs.overlay.style.display = "none";
+                vm.$store.commit("hide");
+            });
+        });
+    },
+
+    beforeRouteLeave(to, from, next) {
+        console.log("่ก่อน ออก จาก Component นี้ ");
+
+        if (this.update_save || this.insert_save) {
+            if (confirm("ข้อมุูลที่แก้ไข ยังไม่ saved ต้องการ saveก่อน หรือไม่ ? ")) {
+                next(false);
+            } else {
+                // this.update_save = false;
+                // this.insert_save = false;
+                next(vm => {
+                    // vm.$roo.$refs.overlay.style.display = "block";
+                });
+            }
+        } else {
+            //   this.update_save = false;
+            //   this.insert_save = false;
+            next(vm => {
+                // vm.$roo.$refs.overlay.style.display = "block";
+            });
+        }
+    },
     mixins: [],
     data() {
         return {
